137e396a8b1e6e20cabae3f3336e3c32087d6056
Juan carlos <juancarloscruz278@gmail.com>
2020-11-10 00:09:20 -0400

    Feature: Enhance slider to react to next and prev and display the current slider depending on the click.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ let userName = document.querySelector('.profile_name_main');
 let userTitle =  document.querySelector('.profile_name_title');
 let slideContainer = document.querySelector('.profile-img-container');
 let userProfileName = document.querySelector('.profile_name');
-let slide = parseInt(slideContainer.dataset.slide);
+// let slide = parseInt(slideContainer.dataset.slide);
+let index = 0;
 let data = [
     {
         name: "Tanya Sinclair",
@@ -19,44 +20,72 @@ let data = [
         title: "Junior Front-end Developer",
         description: "“ If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I noew feel so confident about starting up as a professional developer. ”",
         image: "./images/image-john.jpg"
+    },
+    {
+        name: "Leah Chatman",
+        title: "Mom & Backend Developer",
+        description: "“ Changing my job from teacher to Backend developer has made a huge impact in my life. Now I work remotely for Microsoft and can have more time with them and can help them in anything. ”",
+        image: "./images/image-teacher.jpeg"
+    },
+    {
+        name: "Joshua Wilson",
+        title: "Teacher & Developer",
+        description: "“ If you really want to learn to code and create great things, you have to take this course and commit yourself to finish it and I you will see the results along the way as you finish each topic. ”",
+        image: "./images/image-programmer.jpeg"
     }
+
+
 ]
 document.addEventListener('DOMContentLoaded', () => {
-    userImage.src = data[slide].image;
-    userName.innerHTML = data[slide].name;
-    userTitle.innerHTML = data[slide].title;
-    userDescription.innerHTML = data[slide].description
+    userImage.src = data[0].image;
+    userName.innerHTML = data[0].name;
+    userTitle.innerHTML = data[0].title;
+    userDescription.innerHTML = data[0].description
 })
 document.addEventListener('keydown', (e) => {
     if(e.keyCode === 37){
-        changeUserInfo("0", "0")
+        changeUserInfo()
     }
     else if(e.keyCode === 39){
-        changeUserInfo("1", "1")
+        changeUserInfo()
     }
 })
-function changeUserInfo(sliderInfo, setSlider){
-    //Only execute animation if the data attribute is different
-    if(slideContainer.dataset.slide !== sliderInfo){
-        slideContainer.dataset.slide = setSlider;
-        userImage.classList.add('hide');
-        userDescription.classList.add('hide');
-        userProfileName.classList.add('hide');
-        setTimeout(() => {
-        userImage.src = data[setSlider].image;
-        userName.innerHTML = data[setSlider].name;
-        userTitle.innerHTML = data[setSlider].title;
-        userDescription.innerHTML = data[setSlider].description;
+function displayData(index){
+    setTimeout(() => {
+        userImage.src = data[index].image;
+        userName.innerHTML = data[index].name;
+        userTitle.innerHTML = data[index].title;
+        userDescription.innerHTML = data[index].description;
         userImage.classList.remove('hide');
         userDescription.classList.remove('hide');
         userProfileName.classList.remove('hide')
     }, 500)
+}
+function changeUserInfo(type){
+    userImage.classList.add('hide');
+    userDescription.classList.add('hide');
+    userProfileName.classList.add('hide');
+    //Logic to change the slide depending on asc or desc mode.
+    if(type == 'desc'){
+        if(index === 0){
+            index = data.length-1;
+        }else{
+            index -= 1;
+        }
+    }else{
+        // ascend mode
+        if(index < data.length -1){
+            index++;
+        }else{
+         index = 0;
+        }
     }
+    displayData(index)
 }
 prevtButton.addEventListener('click', () => {
-    changeUserInfo("0", "0")
+    changeUserInfo('desc')
     
 })
 nextButton.addEventListener('click', () => {
-    changeUserInfo("1", "1")
+    changeUserInfo('asc')
 })
